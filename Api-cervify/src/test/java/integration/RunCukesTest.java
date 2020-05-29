b6ac8c1edb6650e9290638c6a401504d4fa07d81
Fennema lucas <ld.fennema@gmail.com>
2020-05-28 23:36:23 -0300

    ci segundo intento

--- Api-cervify/src/test/java/integration/RunCukesTest.java
+++ Api-cervify/src/test/java/integration/RunCukesTest.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         format={"pretty"},
-        features= "src/test/resources/")
+        features= "src/test/resources/"        )
 public class RunCukesTest {
 }
