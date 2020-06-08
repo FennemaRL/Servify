63b4968be62024968f97d41cbe34c325f89f0d03
Fennema lucas <ld.fennema@gmail.com>
2020-06-08 16:42:48 -0300

    se agrega usuario remoto

--- webservify/src/testadd.js
+++ webservify/src/testadd.js
@@ -5,7 +5,6 @@ import axios from 'axios';
 
 const HorizontalLoginForm = () => {
   const [form] = Form.useForm();
-  const [form1] = Form.useForm();
   const [, forceUpdate] = useState(); // To disable submit button at the beginning.
 
   useEffect(() => {
@@ -13,15 +12,8 @@ const HorizontalLoginForm = () => {
   }, []);
 
   const onFinish = values => {
-    axios.post(`${process.env.REACT_APP_API_URL}/api/provider/service`,{data:{values:{ username:"Test",...values}}})
+    axios.post(`${process.env.REACT_APP_API_URL}/api/provider`,{data:{values:{ username:values.username}}})
       .then(res=> {alert("se agrego con exito")
-                   console.log(res.data)})
-      .catch(err=>alert(err.response.data))
-  };
-  const onFinishDelete = values => {
-    console.log(values)
-    axios.delete(`${process.env.REACT_APP_API_URL}/api/provider/serviced`,{data:{values:{ username:"Test",...values}}})
-      .then(res=> {alert("se borro con exito")
                    console.log(res.data)})
       .catch(err=>alert(err.response.data))
   };
@@ -31,7 +23,7 @@ const HorizontalLoginForm = () => {
       <h2>Agregar Categoria</h2>
     <Form form={form} name="horizontal_login" layout="inline" onFinish={onFinish}>
       <Form.Item
-        name="category"
+        name="username"
         rules={[
           {
             required: true,
@@ -39,7 +31,7 @@ const HorizontalLoginForm = () => {
           },
         ]}
       >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="category" />
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="username" />
       </Form.Item>
       <Form.Item shouldUpdate>
         {() => (
@@ -55,36 +47,7 @@ const HorizontalLoginForm = () => {
           </Button>
         )}
       </Form.Item>
-    </Form>
-    <h2>Eliminar Categoria</h2>
-    <Form form={form1} name="horizontal_login" layout="inline" onFinish={onFinishDelete}>
-      <Form.Item
-        name="category"
-        rules={[
-          {
-            required: true,
-            message: 'Please input category',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="category" />
-      </Form.Item>
-      <Form.Item shouldUpdate>
-        {() => (
-          <Button
-            type="primary"
-            htmlType="submit"
-            disabled={
-              !form1.isFieldsTouched(true) ||
-              form1.getFieldsError().filter(({ errors }) => errors.length).length
-            }
-          >
-            Delete Category
-          </Button>
-        )}
-      </Form.Item>
-    </Form>
-    
+    </Form>    
     </div>
   );
 };
