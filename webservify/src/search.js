12da73185225cf8892af13b2167063665c40fc30
ignacio <ignaciorobledounq@gmail.com>
2020-06-13 19:35:05 -0300

    agrego redireccion a la vista de un proveedor en la busqueda de servicios

--- webservify/src/search.js
+++ webservify/src/search.js
@@ -1,31 +1,38 @@
-import React,{useState, useEffect} from 'react';
-import { useParams } from "react-router-dom";
-import { Typography, List, Avatar } from 'antd';
+import React, {useEffect, useState} from 'react';
+import {NavLink, useParams} from "react-router-dom";
+import {Avatar, List, Typography} from 'antd';
 import axios from "axios";
-const { Title } = Typography;
-function Search(){
-    let { category } = useParams();
+
+const {Title} = Typography;
+
+function Search() {
+    let {category} = useParams();
     const [providers, setCategories] = useState([]);
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_URL}/api/services/${category}`)
-          .then(res=>{setCategories(res.data)
-                        console.log(res.data)})
-          .catch(err=>console.error(err))
-      },[category]);
-    
-return (
-    <div style={{display:'flex',flexDirection:'column', justifyContent:'center',alignItems:'center'}}>
-        <Title  style={{textAlign:'center'}} level={2}>Búsqueda {category}</Title>
-        <List itemLayout="horizontal" dataSource={providers} size='large' renderItem={item => (
-            <List.Item style={{backgroundColor:'#d9d9d9', borderRadius:5}}>
-            <List.Item.Meta
-                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                title={<p>{item.username}</p>}
-                description={item.description? item.description:"No contiene descripcion"}
+            .then(res => {
+                setCategories(res.data)
+                console.log(res.data)
+            })
+            .catch(err => console.error(err))
+    }, [category]);
+
+    return (
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <Title style={{textAlign: 'center'}} level={2}>Búsqueda {category}</Title>
+            <List itemLayout="horizontal" dataSource={providers} size='large' renderItem={item => (
+                <List.Item style={{backgroundColor: '#d9d9d9', borderRadius: 5}}>
+                    <NavLink to={`/Servify/view/${item.username}`}>
+                        <List.Item.Meta
+                            avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>}
+                            title={<p>{item.username}</p>}
+                            description={item.description ? item.description : "No contiene descripcion"}
+                        />
+                    </NavLink>
+                </List.Item>
+            )}
             />
-      </List.Item>
-    )}
-  />
-</div>)
+        </div>)
 }
+
 export default Search
